fd1375619bd6a4cf057213c48e7520439aeb2f20
Damien Pellerin <damienpellerin19@gmail.com>
2022-09-27 15:32:13 +0200

    v2

---
+++ controllers/home_controller.php
@@ -0,0 +1,18 @@
+<?php 
+require_once(__DIR__.'/../config/config.php');
+include(__DIR__.'/../views/templates/header.php');
+//DONNÉES RECU EN METHOD POST//
+include(__DIR__.'/../views/home.php');
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $choices = filter_input(INPUT_POST, 'choice', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY) ?? [];
+    if (count($choices) < 2 || 3 > count($choices) ) {
+        echo 'Donée non valide';
+    }else{
+        
+       
+    }
+
+}
